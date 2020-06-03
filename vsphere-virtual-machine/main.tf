e39191d5efd1c0147d0624b40bd7e0462ab964bd
bykvaadm <info@bykvaadm.ru>
2020-06-04 01:21:16 +0300

    Update main.tf

--- vsphere-virtual-machine/main.tf
+++ vsphere-virtual-machine/main.tf
@@ -7,7 +7,7 @@ data "vsphere_tag_category" "Environment" {
 }
 
 data "vsphere_tag" "Environment" {
-  name        = "PROD"
+  name        = var.environment
   category_id = data.vsphere_tag_category.Environment.id
 }
 
@@ -30,7 +30,8 @@ data "vsphere_tag" "CostCenterCode" {
 }
 
 data "vsphere_datastore" "datastore" {
-  name          = var.vsphere_datastore_name
+  count         = length(var.datastore_name)
+  name          = element(var.datastore_name, count.index)
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
@@ -72,18 +73,63 @@ locals {
   list(data.vsphere_tag.CostCenterCode.id))
 }
 
+resource "kubernetes_endpoints" "prometheus" {
+  count = length(var.prometheus) == 0 ? 0 : var.instance_count
+  metadata {
+    namespace = "headless"
+    name      = "${var.prefix}${var.name}-${count.index}"
+  }
+
+  subset {
+    address {
+      ip = var.network_address[0][count.index]
+    }
+    dynamic "port" {
+      for_each = [for s in var.prometheus: {
+        name     = s.name
+        port     = s.port
+        protocol = s.protocol
+      }]
+      content {
+        name     = port.value.name
+        port     = port.value.port
+        protocol = port.value.protocol
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "prometheus" {
+  count = length(var.prometheus) == 0 ? 0 : var.instance_count
+  metadata {
+    namespace   = "headless"
+    name        = "${var.prefix}${var.name}-${count.index}"
+    labels      = {
+      service = var.name
+    }
+    annotations = {
+      "prometheus.io/scrape" = "true"
+    }
+  }
+  spec {
+    cluster_ip = "None"
+    type       = "ClusterIP"
+  }
+}
+
+
 resource "vsphere_virtual_machine" "vm" {
   name             = "${var.prefix}${var.name}-${count.index}"
-  count            = var.with_vm_clone == true ? var.instance_count : 0
+  count            = var.with_vm_clone == true && var.operation_system == "linux" ? var.instance_count : 0
   tags             = var.uniq_id_tag_count != 0 ? concat(local.tags, list(element(vsphere_tag.uniq_id_tag, count.index).id)) : local.tags
   resource_pool_id = data.vsphere_resource_pool.pool.id
-  datastore_id     = data.vsphere_datastore.datastore.id
+  datastore_id     = element(data.vsphere_datastore.datastore.*.id, count.index)
   enable_disk_uuid = "true"
 
   num_cpus               = var.num_cpus
   cpu_hot_add_enabled    = true
   memory_hot_add_enabled = true
-  memory_reservation     = var.memory
+  memory_reservation     = var.memory_reservation != 0 ? var.memory_reservation : var.memory / 2
   memory                 = var.memory
 
   guest_id = var.guest_id
@@ -92,7 +138,8 @@ resource "vsphere_virtual_machine" "vm" {
 
   lifecycle {
     ignore_changes = [
-      clone[0].customize]
+      clone[0].customize,
+      custom_attributes]
   }
 
   dynamic "network_interface" {
@@ -104,17 +151,21 @@ resource "vsphere_virtual_machine" "vm" {
     }
   }
 
+  scsi_type = var.scsi_type
+
   dynamic "disk" {
     for_each = [for d in var.disk_size: {
-      size   = d.size
-      number = d.number
+      size             = d.size
+      number           = d.number
+      eagerly_scrub    = d.eagerly_scrub
+      thin_provisioned = d.thin_provisioned
     }]
 
     content {
       label            = format("disk%s", disk.value.number)
       size             = disk.value.size
-      thin_provisioned = data.vsphere_virtual_machine.template.0.disks[0].thin_provisioned
-      eagerly_scrub    = data.vsphere_virtual_machine.template.0.disks[0].eagerly_scrub
+      thin_provisioned = disk.value.thin_provisioned
+      eagerly_scrub    = disk.value.eagerly_scrub
       unit_number      = disk.value.number
     }
   }
@@ -143,29 +194,29 @@ resource "vsphere_virtual_machine" "vm" {
   }
 }
 
-resource "vsphere_virtual_machine" "blank_vm" {
-  name                        = "${var.prefix}${var.name}-${count.index}"
-  count                       = var.with_vm_clone == true ? 0 : var.instance_count
-  tags                        = var.uniq_id_tag_count != 0 ? concat(local.tags, list(element(vsphere_tag.uniq_id_tag, count.index).id)) : local.tags
-  resource_pool_id            = data.vsphere_resource_pool.pool.id
-  datastore_id                = data.vsphere_datastore.datastore.id
-  enable_disk_uuid            = "true"
-  wait_for_guest_ip_timeout   = 0
-  wait_for_guest_net_routable = false
-  wait_for_guest_net_timeout  = 0
+resource "vsphere_virtual_machine" "windows_vm" {
+  name             = "${var.prefix}${var.name}-${count.index}"
+  count            = var.with_vm_clone == true && var.operation_system == "windows" ? var.instance_count : 0
+  tags             = var.uniq_id_tag_count != 0 ? concat(local.tags, list(element(vsphere_tag.uniq_id_tag, count.index).id)) : local.tags
+  resource_pool_id = data.vsphere_resource_pool.pool.id
+  datastore_id     = element(data.vsphere_datastore.datastore.*.id, count.index)
+  enable_disk_uuid = "true"
+  firmware         = var.firmware
 
   num_cpus               = var.num_cpus
   cpu_hot_add_enabled    = true
   memory_hot_add_enabled = true
-  memory_reservation     = var.memory
+  memory_reservation     = var.memory / 2
   memory                 = var.memory
 
   guest_id = var.guest_id
-  folder   = var.vsphere_vm_folder
 
+  folder = var.vsphere_vm_folder
 
-  cdrom {
-    client_device = true
+  lifecycle {
+    ignore_changes = [
+      clone[0].customize,
+      custom_attributes]
   }
 
   dynamic "network_interface" {
@@ -181,15 +232,99 @@ resource "vsphere_virtual_machine" "blank_vm" {
 
   dynamic "disk" {
     for_each = [for d in var.disk_size: {
-      size   = d.size
-      number = d.number
+      size             = d.size
+      number           = d.number
+      eagerly_scrub    = d.eagerly_scrub
+      thin_provisioned = d.thin_provisioned
     }]
 
     content {
       label            = format("disk%s", disk.value.number)
       size             = disk.value.size
-      thin_provisioned = true
-      eagerly_scrub    = false
+      thin_provisioned = disk.value.thin_provisioned
+      eagerly_scrub    = disk.value.eagerly_scrub
+      unit_number      = disk.value.number
+    }
+  }
+
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template.0.id
+
+    customize {
+      windows_options {
+        computer_name = "${var.prefix}${var.name}-${count.index}"
+      }
+      dynamic "network_interface" {
+        for_each = [for n in var.network_address: {
+          ip = n[count.index]
+        }]
+        content {
+          ipv4_address = network_interface.value.ip
+          ipv4_netmask = var.ipv4_netmask
+        }
+      }
+
+      ipv4_gateway    = var.ipv4_gateway
+      dns_server_list = var.dns_server_list
+    }
+  }
+}
+
+resource "vsphere_virtual_machine" "blank_vm" {
+  name                        = "${var.prefix}${var.name}-${count.index}"
+  count                       = var.with_vm_clone == true ? 0 : var.instance_count
+  tags                        = var.uniq_id_tag_count != 0 ? concat(local.tags, list(element(vsphere_tag.uniq_id_tag, count.index).id)) : local.tags
+  resource_pool_id            = data.vsphere_resource_pool.pool.id
+  datastore_id                = element(data.vsphere_datastore.datastore.*.id, count.index)
+  enable_disk_uuid            = "true"
+  wait_for_guest_ip_timeout   = 0
+  wait_for_guest_net_routable = false
+  wait_for_guest_net_timeout  = 0
+
+  num_cpus               = var.num_cpus
+  cpu_hot_add_enabled    = true
+  memory_hot_add_enabled = true
+  memory_reservation     = var.memory / 2
+  memory                 = var.memory
+
+  guest_id = var.guest_id
+  folder   = var.vsphere_vm_folder
+
+  lifecycle {
+    ignore_changes = [
+      custom_attributes]
+  }
+
+  cdrom {
+    client_device = var.cdrom_datastore_id == "" ? true : false
+    datastore_id  = var.cdrom_datastore_id
+    path          = var.cdrom_iso_path
+  }
+
+  dynamic "network_interface" {
+    for_each = [for n in data.vsphere_network.network: {
+      id = n.id
+    }]
+    content {
+      network_id = network_interface.value.id
+    }
+  }
+
+  scsi_type = var.scsi_type
+
+  dynamic "disk" {
+    for_each = [for d in var.disk_size: {
+      size             = d.size
+      number           = d.number
+      eagerly_scrub    = d.eagerly_scrub
+      thin_provisioned = d.thin_provisioned
+    }]
+
+    content {
+      label            = format("disk%s", disk.value.number)
+      size             = disk.value.size
+      thin_provisioned = disk.value.thin_provisioned
+      eagerly_scrub    = disk.value.eagerly_scrub
       unit_number      = disk.value.number
     }
   }
